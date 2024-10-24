64bc521fa4fa0e5e8597f8c5ffc407ed288f307b
ksenia_02 <vodopyanova02@bk.ru>
2024-10-24 16:21:36 +0300

    Snake movement

--- field.js
+++ field.js
@@ -1,14 +1,7 @@
-const Snake = {
-    initial_X: 0,
-    initial_Y: 2,
-    finally_X: 0,
-    finally_Y: 0,
-}
-
 const searchPlayingField = document.querySelector('.playing__field')
 
 const field = [
-    [0,0,1,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
@@ -16,7 +9,9 @@ const field = [
     [0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0],
 ];
-console.log(field[3][4])
+
+// console.log(field[3][4])
+
 function drowUi(){
     for (let i=0; i<field.length; i++){
         const rowsField = document.createElement("div")
@@ -26,6 +21,11 @@ function drowUi(){
             rowsItem.className = `rows-item` 
             if(field[i][j] === 0) {
                 rowsItem.style.backgroundColor='green'
+                rowsField.append(rowsItem)
+                continue;
+            }
+            if(field[i][j] === 2) {
+                rowsItem.style.backgroundColor='red'
                 rowsField.append(rowsItem)
                 continue;
             }
@@ -39,24 +39,127 @@ function drowUi(){
 }
 drowUi()
 
+const Snake = [
+    { header:{
+        x: 4,
+        y: 5,
+    }},
+    {positionBody:{
+        x: 5,
+        y: 5,
+    }},
+
+];
+
+function drawingSnake(x, y){
+    field[x][y] = 1
+    rerender()
+}
+
+drawingSnake(Snake[0].header.x, Snake[0].header.y)
+drawingSnake(Snake[1].positionBody.x, Snake[1].positionBody.y)
+
+
+
+
+
+
+
+
 
 function rerender(){
     searchPlayingField.innerHTML='';
     drowUi()
 }
 
+
+
+
+
 document.addEventListener('keydown', function(event) {
-    if (event.code == 'KeyS') {
-      Snake.finally_X = Snake.initial_X + 1
-      field[Snake.initial_X][Snake.initial_Y] = 0
-      field[Snake.finally_X][Snake.initial_Y] = 1
-      Snake.initial_X = Snake.finally_X
-      rerender()
-    }
-    console.log(Snake.initial_X)
+    switch (event.code) {
+        case 'KeyS':
+            // field[Snake[1].header.x][Snake[1].header.y] = 0
+            // Snake[0].positionBody.x = Snake[0].positionBody.x + 1
+            // field[Snake[0].positionBody.x][Snake[0].positionBody.y] = 1
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
+            Snake[1].positionBody.x =  Snake[0].header.x
+            Snake[1].positionBody.y =  Snake[0].header.y
+            Snake[0].header.x = Snake[0].header.x + 1
+            field[Snake[0].header.x][Snake[0].header.y] = 1
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
+// console.log("Это х", Snake[0].header.x)
+// console.log("Это у", Snake[0].header.y)
+// console.log("Это след коор",field[(Snake[0].header.x)+1][Snake[0].header.y])
+
+// if(field[(Snake[0].positionBody.x)+1][Snake[0].positionBody.y] === 2){
+//     field[(Snake[0].positionBody.x)+1][Snake[0].positionBody.y] = 1
+// }
+            rerender()
+            break;
+
+
+        case 'KeyW':
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
+            Snake[1].positionBody.x =  Snake[0].header.x
+            Snake[1].positionBody.y =  Snake[0].header.y
+            Snake[0].header.x = Snake[0].header.x - 1
+            field[Snake[0].header.x][Snake[0].header.y] = 1
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
+            console.log("голова", [Snake[0].header.x],":",[Snake[0].header.y])
+            console.log("хвост", [Snake[1].positionBody.x],":",[Snake[1].positionBody.y])
+
+            rerender()
+            break;
+        
+        
+        case 'KeyD':
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
+            Snake[1].positionBody.y =  Snake[0].header.y
+            Snake[1].positionBody.x =  Snake[0].header.x
+            Snake[0].header.y = Snake[0].header.y + 1
+            field[Snake[0].header.x][Snake[0].header.y] = 1
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
+            rerender()
+            break;
+        case 'KeyA':
+
+
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
+            Snake[1].positionBody.y =  Snake[0].header.y
+            Snake[1].positionBody.x =  Snake[0].header.x
+            Snake[0].header.y = Snake[0].header.y - 1
+            field[Snake[0].header.x][Snake[0].header.y] = 1
+            field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
+
+
+            console.log("голова", [Snake[0].header.x],":",[Snake[0].header.y])
+            console.log("хвост", [Snake[1].positionBody.x],":",[Snake[1].positionBody.y])
+        rerender()
+        break;
+      }
+
+    console.log([Snake.finally_X], Snake.finally_Y)
   });
+
+
+
+
+
+
+
+
 
 // const snake = (x, y) => {
 //     field[x][y] = 1
 //     rerender()
 // }
+
+
+    // if (event.code == 'KeyS') {
+    //   Snake.finally_X = Snake.initial_X + 1
+    //   field[Snake.initial_X][Snake.initial_Y] = 0
+    //   field[Snake.finally_X][Snake.initial_Y] = 1
+    //   Snake.initial_X = Snake.finally_X
+    //   rerender()
+    // }
