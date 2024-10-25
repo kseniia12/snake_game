e8a1887acf213afd7e62bc4f337e5184adf98be1
ksenia_02 <vodopyanova02@bk.ru>
2024-10-25 16:57:14 +0300

    Key response

--- field.js
+++ field.js
@@ -1,16 +1,21 @@
 const searchPlayingField = document.querySelector('.playing__field')
 
 const field = [
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ];
-
-// console.log(field[3][4])
 
 function drowUi(){
     for (let i=0; i<field.length; i++){
@@ -41,43 +46,38 @@ drowUi()
 
 const Snake = [
     {
-        x: 3,
+        x: 9,
         y: 5,
+        oldX: 9,
+        oldY: 5,
     },
     {
-        x: 4,
+        x: 10,
         y: 5,
+        oldX: 10,
+        oldY: 5,
     },
     {
-        x: 5,
+        x: 11,
         y: 5,
+        oldX: 11,
+        oldY: 5,
     },
     {
-        x: 6,
+        x: 12,
         y: 5,
+        oldX: 12,
+        oldY: 5,
     },
     
 
 ];
-
+console.log(Snake[0].x)
 
 function drawingSnake(x, y){
     field[x][y] = 1
     rerender()
 }
-
-// drawingSnake(Snake[0].header.x, Snake[0].header.y)
-// drawingSnake(Snake[1].positionBody.x, Snake[1].positionBody.y)
-
-
-
-// for (let i=0; i < Snake.length; i++){
-//     for(let j in Snake[i]){
-//         for(let k in Snake[i][j]){
-//             console.log(Snake[i][j][k])
-//         }
-//     }
-// }
 
 
 Snake.forEach((item)=>{
@@ -93,144 +93,82 @@ function rerender(){
 }
 
 
+function walkDown(){
+    Snake.forEach((item, index)=>{
+        item.oldX = item.x
+        item.oldY = item.y
+        if (index === 0){
+            Snake[0].x = Snake[0].x + 1
+        } else{
+            field[Snake[Snake.length-1].x][Snake[Snake.length-1].y] = 0
+            Snake[index].y = Snake[index-1].oldY
+            Snake[index].x = Snake[index-1].oldX
+        }
+        field[item.x][item.y]=1
+    })
+    rerender()
+}
 
 
+function walkUp(){
+    Snake.forEach((item, index)=>{
+        item.oldX = item.x
+        item.oldY = item.y
+        if (index === 0){
+            Snake[0].x = Snake[0].x - 1
+        } else{
+            field[Snake[Snake.length-1].x][Snake[Snake.length-1].y] = 0
+            Snake[index].y = Snake[index-1].oldY
+            Snake[index].x = Snake[index-1].oldX
+        }
+        field[item.x][item.y]=1
+    })
+        rerender()
+}
 
-
+function walkRight(){
+    Snake.forEach((item, index)=>{
+        item.oldX = item.x
+        item.oldY = item.y
+        if (index === 0){
+            Snake[0].y = Snake[0].y + 1
+        } else{
+            field[Snake[Snake.length-1].x][Snake[Snake.length-1].y] = 0
+            Snake[index].y = Snake[index-1].oldY
+            Snake[index].x = Snake[index-1].oldX
+        }
+        field[item.x][item.y]=1
+    })
+    rerender()
+}
+function walkLeft(){
+    Snake.forEach((item, index)=>{
+        item.oldX = item.x
+        item.oldY = item.y
+        if (index === 0){
+            Snake[0].y = Snake[0].y - 1
+        } else{
+            field[Snake[Snake.length-1].x][Snake[Snake.length-1].y] = 0
+            Snake[index].y = Snake[index-1].oldY
+            Snake[index].x = Snake[index-1].oldX
+        }
+        field[item.x][item.y]=1
+    })
+    rerender()
+}
     document.addEventListener('keydown', function(event) {
         switch (event.code) {
             case 'KeyS':
-                Snake.forEach((item, index)=>{
-                    if(index === 0){
-                        field[item.x][item.y] = 0
-                    }
-                    item.x = item.x + 1
-                    field[item.x][item.y] = 1
-                  
-                })
-       
-              
-                // field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
-                // Snake[1].positionBody.x =  Snake[0].header.x
-                // Snake[1].positionBody.y =  Snake[0].header.y
-                // Snake[0].header.x = Snake[0].header.x + 1
-                // field[Snake[0].header.x][Snake[0].header.y] = 1
-                // field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
-    
-                rerender()
+                walkDown()
                 break;
-    
-    
             case 'KeyW':
-
-            Snake.forEach((item, index)=>{
-                if(index === Snake.length-1){
-                    field[item.x][item.y] = 0
-                }
-                item.x = item.x - 1
-                field[item.x][item.y] = 1
-              
-            })
-
-                // field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
-                // Snake[1].positionBody.x =  Snake[0].header.x
-                // Snake[1].positionBody.y =  Snake[0].header.y
-                // Snake[0].header.x = Snake[0].header.x - 1
-                // field[Snake[0].header.x][Snake[0].header.y] = 1
-                // field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
-                // console.log("голова", [Snake[0].header.x],":",[Snake[0].header.y])
-                // console.log("хвост", [Snake[1].positionBody.x],":",[Snake[1].positionBody.y])
-    
-                rerender()
+                walkUp()
                 break;
-            
-            
             case 'KeyD':
-                Snake.forEach((item, index)=>{
-                    if(index === Snake.length-1){
-                        field[item.x][item.y] = 0
-                    }
-                    if(index === 0){
-                        field[item.x][item.y] = 0
-                        item.y = item.y + 1
-                    }
-                    else{
-                        item.x = item.x - 1
-                    }
-                    
-                    field[item.x][item.y] = 1
-                  
-                })
-                // item.x = item.x - 1
-                // field[item.x][item.y] = 1
-                // field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
-                // Snake[1].positionBody.y =  Snake[0].header.y
-                // Snake[1].positionBody.x =  Snake[0].header.x
-                // Snake[0].header.y = Snake[0].header.y + 1
-                // field[Snake[0].header.x][Snake[0].header.y] = 1
-                // field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
-                rerender()
+                walkRight()
                 break;
-            // case 'KeyA':
-    
-    
-            //     field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 0
-            //     Snake[1].positionBody.y =  Snake[0].header.y
-            //     Snake[1].positionBody.x =  Snake[0].header.x
-            //     Snake[0].header.y = Snake[0].header.y - 1
-            //     field[Snake[0].header.x][Snake[0].header.y] = 1
-            //     field[Snake[1].positionBody.x][Snake[1].positionBody.y] = 1
-    
-    
-            //     console.log("голова", [Snake[0].header.x],":",[Snake[0].header.y])
-            //     console.log("хвост", [Snake[1].positionBody.x],":",[Snake[1].positionBody.y])
-            // rerender()
-            // break;
+            case 'KeyA':
+                walkLeft()
+            break;
           }
-    
-        console.log([Snake.finally_X], Snake.finally_Y)
       });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const snake = (x, y) => {
-//     field[x][y] = 1
-//     rerender()
-// }
-
-
-    // if (event.code == 'KeyS') {
-    //   Snake.finally_X = Snake.initial_X + 1
-    //   field[Snake.initial_X][Snake.initial_Y] = 0
-    //   field[Snake.finally_X][Snake.initial_Y] = 1
-    //   Snake.initial_X = Snake.finally_X
-    //   rerender()
-    // }
-
-
-            // field[Snake[1].header.x][Snake[1].header.y] = 0
-            // Snake[0].positionBody.x = Snake[0].positionBody.x + 1
-            // field[Snake[0].positionBody.x][Snake[0].positionBody.y] = 1
-
-
-
-    // console.log("Это х", Snake[0].header.x)
-// console.log("Это у", Snake[0].header.y)
-// console.log("Это след коор",field[(Snake[0].header.x)+1][Snake[0].header.y])
-
-// if(field[(Snake[0].positionBody.x)+1][Snake[0].positionBody.y] === 2){
-//     field[(Snake[0].positionBody.x)+1][Snake[0].positionBody.y] = 1
-// }
